966b3173360ade1fa433dddc535dc7ade89bfacb
YuzuruSano <ysano@oikaze.jp>
2019-10-12 19:35:59 +0900

    fix 書籍提供のライブラリを扱えるよう修正

--- dev/js/scripts/main.js
+++ dev/js/scripts/main.js
@@ -1,27 +1,23 @@
 import Entry from "entry";
 import BrowserDetect from "modules/BrowserDetect";
+import * as gd from "generative-design-library.js";
 import * as p5 from 'p5';
 
 let s = (sk) => {
+  
+
+  sk.preload = () => {
+    
+  }
+
   sk.setup = () => {
-    sk.createCanvas(720, 720);
-    sk.noFill();
+    
   }
 
   sk.draw = () => {
-    sk.background(255);
-    sk.ellipse(sk.pmouseX, sk.pmouseY, 40, 40);
-
-    sk.fill(128);
-    sk.strokeWeight(1);
-    sk.ellipse(50,40, 40, 40);
-    sk.rect(50,50,40,20);
+    
   }
-}
 
+  
+}
 const P5 = new p5(s);
-
-
-if (module.hot) {
-  module.hot.accept();
-}

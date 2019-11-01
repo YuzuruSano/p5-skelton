8a5117b142bbc43e734f4e8c0222f930eb40dc63
YuzuruSano <ysano@oikaze.jp>
2019-11-02 07:47:27 +0900

    update パスに沿って這わせる基本処理、class分割

--- dev/js/scripts/main.js
+++ dev/js/scripts/main.js
@@ -1,51 +1,25 @@
 import Entry from "entry";
 import BrowserDetect from "modules/BrowserDetect";
 import SortColors from "modules/SortColors";
+import SVG from "modules/SVG";
+import ImgPos from "modules/ImgPos";
 import * as gd from "generative-design-library.js";
 import * as p5 from 'p5';
 import 'p5/lib/addons/p5.sound';
 import 'p5/lib/addons/p5.dom';
-import axios from 'axios';
 import { createNoSubstitutionTemplateLiteral } from "typescript";
 
-const getSvgInfo = () => {
-  const svgElement = document.querySelector('svg');
-  const viewBoxParams = svgElement.getAttribute('viewBox').split(' ').map((param) => +param);
-  return {
-    element: svgElement,
-    viewBox: {
-      x: viewBoxParams[0],
-      y: viewBoxParams[1],
-      width: viewBoxParams[2],
-      height: viewBoxParams[3]
-    }
-  };
-}
-
-const getFile = (file) => {
-  return axios.get(file)
-  .then(function (response) {
-    const svg = response.data;
-    document.body.insertAdjacentHTML('beforeend', svg);
-  })
-  .then(() => {
-    const info = getSvgInfo();
-    return info;
-  })
-  .catch(function (error) {
-    console.log(error);
-  })
-}
+const svg = new SVG();
+const imgPos = new ImgPos();
 
 let s = (sk) => {
-  let tileCount = 10;
-  let actRandomSeed = 0;
-  let actStrokeCap;
   let svgInfo;
-  let path;
-  let nowPathPosition = 0; // 現在のSVG pathの位置
+  let nowPathPosition = 0;
   let maxPathLength;
-  let pathElement;
+
+  let prevX = 0;
+  let prevY = 0;
+
   let creature;
 
   let isJumping = false;
@@ -54,14 +28,13 @@ let s = (sk) => {
   let jumpMax = 300;
 
   sk.setup = () => {
-    sk.createCanvas(1000, 1000);
-    actStrokeCap = sk.ROUND;
+    const canvas = sk.createCanvas(sk.windowWidth, sk.windowHeight);
+    canvas.parent('canvas');
     creature = sk.createImg("../images/creature.gif");
 
-    getFile('../images/svg.svg').then(data => {
+    svg.getFile('../images/svg.svg').then(data => {
       svgInfo = data;
-      pathElement = data.element.querySelector('path');
-      maxPathLength = pathElement.getTotalLength();
+      maxPathLength = svgInfo.element.querySelector('path').getTotalLength();
     });
   }
 
@@ -70,48 +43,27 @@ let s = (sk) => {
     const height = sk.height;
     const mouseX = sk.pmouseX;
     const mouseY = sk.pmouseY;
-    const SPEED = sk.random(10, 500);
+    const speed = sk.random(10, 500);
     const randomX = sk.random(10, 15);
     const randomY = sk.random(10, 15);
 
-    // 現在の座標がpathの全長を越していたら0に初期化
-    if (nowPathPosition > maxPathLength) {
-      nowPathPosition = 0;
-    }
-
-    // 次の位置に進める
-    nowPathPosition = (nowPathPosition + SPEED / 100);
-    let roatateBase1 = nowPathPosition - 10 ;//現在の位置からのラジアン計算ポイント1 数値を増やすと緩やかな変化に
-    let roatateBase2 = nowPathPosition + 10 ;//現在の位置からのラジアン計算ポイント2 数値を増やすと緩やかな変化に
-    if (roatateBase1 < 0) {
-      roatateBase1 = 0;
-    }
-    if (roatateBase2 > maxPathLength) {
-      roatateBase2 = maxPathLength;
-    }
-
-    if (pathElement){
-      const pt1 = pathElement.getPointAtLength(nowPathPosition);
-      const pt0 = pathElement.getPointAtLength(roatateBase1);
-      const pt2 = pathElement.getPointAtLength(roatateBase2);
-
-      const dx = pt2.x - pt0.x;
-      const dy = pt2.y - pt0.y;
-      let rotate = Math.atan(dy / dx);
+    if (svgInfo){
       
-      if (dx < 0) {
-        rotate += Math.PI;
+      //現在の座標がpathの全長を越していたら0に初期化
+      nowPathPosition = (nowPathPosition + speed / 100);
+      if (nowPathPosition > maxPathLength) {
+        nowPathPosition = 0;
       }
 
-      // DOM上の座標を求める
-      const svgClientRect = svgInfo.element.getBoundingClientRect();
-      let x = (pt1.x * svgClientRect.width / svgInfo.viewBox.width) + randomX;
-      let y = (pt1.y * svgClientRect.height / svgInfo.viewBox.height) + randomY;
-      
+      //画像の現在位置と傾きを取得
+      const p = imgPos.getNowImgPosition(creature, svgInfo, nowPathPosition, prevX, prevY, speed);
+      let { x, y, r, originX, originY} = {...p};
+      sk.rect(originX, originY, 10, 10);
+
       //jump
       if (isJumping){
-        const jump = (rotate < 0) ? rotate - 1.5708 : rotate + 1.5708;
-        const jumpX = sk.sin(jump) * jump0; // 円周上のX座標の位置
+        const jump = (r < 0) ? r - 1.5708 : r + 1.5708;
+        const jumpX = sk.sin(jump) * jump0;
         const jumpY = sk.cos(jump) * jump0;
 
         y = y + jumpY;
@@ -132,20 +84,19 @@ let s = (sk) => {
           up = true;
         } 
       }
-      
+
       //jump
       //to do 角度が負のときにおかしい
       // 勾配がゆるいときにおかしいん
       sk.push();
-      //ask.clear();
-      sk.imageMode(sk.CENTER);
-      creature.style('transform', `rotate(${rotate}rad)`);
+      //sk.clear();
+      creature.style('transform', `rotate(${r}rad) `);
+      creature.style('transform-origin', `center`);
       creature.position(x, y);
       sk.pop();
 
-      sk.rect(x, y, 10, 10);
-
-      
+      prevX = x;
+      prevY = y;
     }
   }
 
